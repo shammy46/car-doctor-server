dc53b71bd40dde9104294cc16fed4a871035dc04
shammy46 <sanjidashammy81@gmail.com>
2024-05-23 21:38:54 +0600

    delete operation handled

--- index.js
+++ index.js
@@ -72,6 +72,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/bookings/:id', async(req, res)=>{
+      const id= req.params.id;
+      const query= {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
